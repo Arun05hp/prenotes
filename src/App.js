76d82303970e89f964570b68a4c1ae18adbe4893
Arun Kumar <kumar05.962013@gmail.com>
2020-12-11 17:20:19 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import Navigation from "./Navigation/Navigation";
-
-import "./App.css";
-
 import Signup from "./components/Auth/Signup";
+import "./App.css";
 
 const App = () => {
   return (
--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Route, Redirect, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import Home from "../components/Home/Home";
 import Profile from "../components/Profile/Profile";
 import ChangePassword from "../components/Auth/ChangePassword";
 import MyUpload from "../components/MyUpload/MyUpload";
+
 const Navigation = () => {
   return (
     <div className="main-container">
@@ -13,7 +14,6 @@ const Navigation = () => {
       <div className="app_container">
         <Switch>
           <Route path="/" exact component={Home} />
-
           <Route path="/myprofile" exact component={Profile} />
           <Route path="/changepassword" exact component={ChangePassword} />
           <Route path="/myuploads" exact component={MyUpload} />
